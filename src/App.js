8e3d1bb1bb5b6096a11063810b69c6ce35144b47
alishmanandhar <alish7manandhar@gmail.com>
2021-12-24 11:42:54 +0545

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import "./styles/index.scss";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Button variant="outline-danger">Alish Form</Button>
       <Router>
           <Routes>
             {/* <Route path="/" element={<Index/>} exact /> */}
